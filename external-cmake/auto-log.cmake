2a6d971bc478acf93f8c3890ca07741296bd0b4f
Yakuho <837768318@qq.com>
2025-06-26 20:01:48 +0800

    feat: set cache avoid pull repeat

--- external-cmake/auto-log.cmake
+++ external-cmake/auto-log.cmake
@@ -1,7 +1,8 @@
 find_package(Git REQUIRED)
 include(FetchContent)
 
-set(FETCHCONTENT_BASE_DIR "${CMAKE_CURRENT_BINARY_DIR}/third-party")
+set(FETCHCONTENT_SOURCE_DIR_EXTERN_AUTOLOG ${CMAKE_CURRENT_BINARY_DIR}/third-party/extern_autolog-src)
+set(FETCHCONTENT_BASE_DIR ${CMAKE_CURRENT_BINARY_DIR}/third-party)
 
 FetchContent_Declare(
         extern_autolog
