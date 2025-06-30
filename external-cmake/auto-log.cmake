06e1cc35fc23d3b7538cc7d009dceb6ebfaeebe7
Yakuho <837768318@qq.com>
2025-06-30 11:10:52 +0800

    fix: cache link bug

--- external-cmake/auto-log.cmake
+++ external-cmake/auto-log.cmake
@@ -1,13 +1,15 @@
 find_package(Git REQUIRED)
 include(FetchContent)
 
-set(FETCHCONTENT_SOURCE_DIR_EXTERN_AUTOLOG ${CMAKE_CURRENT_BINARY_DIR}/third-party/extern_autolog-src)
 set(FETCHCONTENT_BASE_DIR ${CMAKE_CURRENT_BINARY_DIR}/third-party)
+if (EXISTS ${FETCHCONTENT_BASE_DIR}/extern_autolog-src)
+    set(FETCHCONTENT_SOURCE_DIR_EXTERN_AUTOLOG ${FETCHCONTENT_BASE_DIR}/extern_autolog-src)
+endif ()
 
 FetchContent_Declare(
-        extern_autolog
-        PREFIX autolog
-        GIT_REPOSITORY https://github.com/LDOUBLEV/AutoLog.git
-        GIT_TAG        main
+    extern_autolog
+    PREFIX autolog
+    GIT_REPOSITORY https://github.com/LDOUBLEV/AutoLog.git
+    GIT_TAG        main
 )
 FetchContent_MakeAvailable(extern_autolog)
